a02b407f4a9613131fc68b9c0d2489c7f8bc248a
Pho3b <andrea.rinaldi54@gmail.com>
2019-05-29 16:18:27 +0200

    cleaned config.php file

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'host' => 'localhost',
-    'username' => 'andrea',
-    'password' => 'F8pj1ILZDE8GEYCn',
-    'db' => 'assignment_db'
+    'host' => '',
+    'username' => '',
+    'password' => '',
+    'db' => ''
 );
